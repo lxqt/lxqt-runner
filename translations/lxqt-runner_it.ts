bcd9ac8946efae4c3a0d5dc75ef23450a00525c3
stefonarch <stef.binde@riseup.net>
2015-08-29 13:30:42 +0200

    Removed country variant _IT

--- translations/lxqt-runner_it.ts
+++ translations/lxqt-runner_it.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
 <context>
     <name>ConfigureDialog</name>
     <message>
