9f0f1c6bb02b1e8c6ace817216be89b783780044
attuska <attus@enterpriseforever.com>
2015-11-06 21:30:38 +0100

    upd: lxqt-runner_hu.ts

--- translations/lxqt-runner_hu.ts
+++ translations/lxqt-runner_hu.ts
@@ -6,52 +6,52 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Futtató beállítás</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Megjelenés</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation type="unfinished"></translation>
+        <translation>Helyzet:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Show on:</source>
-        <translation type="unfinished"></translation>
+        <translation>Látható:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="56"/>
         <source>Shortcut:</source>
-        <translation type="unfinished"></translation>
+        <translation>Gyorsbillentyű:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="60"/>
         <source>Top edge of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő felső szélén</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Center of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő közepén</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Fókuszált képernyőn</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mindig a %1 képernyőn</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Alapállapot</translation>
     </message>
 </context>
 <context>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation type="unfinished"></translation>
+        <translation>Alkalmazás futtató</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="93"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Beállítás</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="97"/>
         <source>Clear History</source>
-        <translation type="unfinished"></translation>
+        <translation>Előzmények törlése</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="345"/>
         <source>Show/hide runner dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Futtatóablak ki/be</translation>
     </message>
 </context>
 <context>
@@ -82,12 +82,12 @@
     <message>
         <location filename="../providers.cpp" line="443"/>
         <source>History</source>
-        <translation type="unfinished"></translation>
+        <translation>Előzmények</translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="801"/>
         <source>Mathematics</source>
-        <translation type="unfinished"></translation>
+        <translation>Matematika</translation>
     </message>
 </context>
 </TS>
