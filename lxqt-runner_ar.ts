85d9be08a1922585882653314d932177af6c6905
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- lxqt-runner_ar.ts
+++ lxqt-runner_ar.ts
@@ -4,13 +4,9 @@
 <context>
     <name>ConfigureDialog</name>
     <message>
-        <source>LXQt-runner Settings</source>
-        <translation type="vanished">إعدادات مُطلق ريزر</translation>
-    </message>
-    <message>
         <location filename="../../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>إعدادات المُطلق</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.ui" line="20"/>
@@ -20,58 +16,42 @@
     <message>
         <location filename="../../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>تحديد الموضع:</translation>
+        <translation>المكان:</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.ui" line="36"/>
         <source>Show on:</source>
-        <translation>إظهارٌ على:</translation>
+        <translation>أظهر على:</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.ui" line="56"/>
         <source>Shortcut:</source>
-        <translation>رابطٌ مختصر:</translation>
-    </message>
-    <message>
-        <source>Top edge of screen</source>
-        <translation type="vanished">الحافَّة العليا للشَّاشة</translation>
-    </message>
-    <message>
-        <source>Center of screen</source>
-        <translation type="vanished">مركز الشَّاشة</translation>
-    </message>
-    <message>
-        <source>Monitor where the mouse</source>
-        <translation type="vanished">المراقبة عند مؤشر الفأرة</translation>
-    </message>
-    <message>
-        <source>Always on %1 monitor</source>
-        <translation type="vanished">دوماً في شاشة العرض %1</translation>
+        <translation>الاختصار:</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.cpp" line="60"/>
         <source>Top edge of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>أعلى حافّة الشّاشة</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.cpp" line="61"/>
         <source>Center of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>وسط الشّاشة</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation type="unfinished"></translation>
+        <translation>الشّاشة الحاليّة</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>الشّاشة %1 دائمًا</translation>
     </message>
     <message>
         <location filename="../../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>صفّر</translation>
     </message>
 </context>
 <context>
@@ -79,34 +59,22 @@
     <message>
         <location filename="../../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>بادئ التطبيقات</translation>
-    </message>
-    <message>
-        <source>Configure lxqt-runner</source>
-        <translation type="vanished">تهيئة مُطلق نظام ريزر</translation>
-    </message>
-    <message>
-        <source>Clear lxqt-runner History</source>
-        <translation type="vanished">مسح ذاكرة مُنفِّذ برامج ريزر</translation>
-    </message>
-    <message>
-        <source>Press &quot;%1&quot; to see dialog.</source>
-        <translation type="vanished">اضغط &quot;%1&quot; لمشاهدة لوحة الحوار.</translation>
+        <translation>مُطلق التّطبيقات</translation>
     </message>
     <message>
         <location filename="../../dialog.cpp" line="93"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>اضبط</translation>
     </message>
     <message>
         <location filename="../../dialog.cpp" line="97"/>
         <source>Clear History</source>
-        <translation type="unfinished"></translation>
+        <translation>امح التّأريخ</translation>
     </message>
     <message>
         <location filename="../../dialog.cpp" line="349"/>
         <source>Show/hide runner dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>أظهر/أخف حواريّ المُطلق</translation>
     </message>
 </context>
 <context>
@@ -114,16 +82,12 @@
     <message>
         <location filename="../../providers.cpp" line="443"/>
         <source>History</source>
-        <translation>سجلُّ السوابق</translation>
+        <translation>التّأريخ</translation>
     </message>
     <message>
         <location filename="../../providers.cpp" line="801"/>
         <source>Mathematics</source>
-        <translation>الرِّياضيَّات</translation>
-    </message>
-    <message>
-        <source>LXQt Power Management</source>
-        <translation type="vanished">إدارة الطاقة لبيئة ريزر</translation>
+        <translation>الرّياضيّات</translation>
     </message>
 </context>
 </TS>
