c4469b807cd168a3bcda750f90c39ea1a2ad87a5
paiiou <paiiou@free.fr>
2016-10-19 10:18:38 +0200

    Update French translations
    liblxqt, about, notificationd, openssh-askpass, policykit,
    powermanagement, runner, session, sudo

---
+++ lxqt-runner_fr.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../../configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Paramétrage de lxqt-runner</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Apparence</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>Position :</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.ui" line="36"/>
+        <source>Show on:</source>
+        <translation>Montrer au :</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.ui" line="46"/>
+        <source>Show history first</source>
+        <translation>Afficher d&apos;abord l&apos;historique</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.ui" line="63"/>
+        <source>Shortcut:</source>
+        <translation>Raccourci :</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.cpp" line="60"/>
+        <source>Top edge of the screen</source>
+        <translation>Le bord supérieur de l&apos;écran</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.cpp" line="61"/>
+        <source>Center of the screen</source>
+        <translation>Le centre de l&apos;écran</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation>Écran ayant le focus</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation>Toujours sur l&apos;écran %1</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation>Réinitialiser</translation>
+    </message>
+</context>
+</TS>
