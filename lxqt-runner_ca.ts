81f54eeea49338b28294001560e0103200f10ac9
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-01-22 17:40:24 +0100

    Update Catalan translation

---
+++ lxqt-runner_ca.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Ajusts de l&apos;executor</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Aparença</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>Posicionament:</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.ui" line="36"/>
+        <source>Show on:</source>
+        <translation>Mostra a:</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.ui" line="56"/>
+        <source>Shortcut:</source>
+        <translation>Drecera:</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.cpp" line="60"/>
+        <source>Top edge of the screen</source>
+        <translation>Vora superior de la pantalla</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Center of the screen</source>
+        <translation>Centre de la pantalla</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation>Pantalla amb focus</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation>Sempre a la panalla %1</translation>
+    </message>
+    <message>
+        <location filename="../../configuredialog/configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation>Restableix</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation>Llançador d&apos;aplicacions </translation>
+    </message>
+    <message>
+        <location filename="../../dialog.cpp" line="93"/>
+        <source>Configure</source>
+        <translation>Configura</translation>
+    </message>
+    <message>
+        <location filename="../../dialog.cpp" line="97"/>
+        <source>Clear History</source>
+        <translation>Neteja l&apos;historial</translation>
+    </message>
+    <message>
+        <location filename="../../dialog.cpp" line="349"/>
+        <source>Show/hide runner dialog</source>
+        <translation>Mostra o oculta el diàleg de l&apos;executor</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../providers.cpp" line="443"/>
+        <source>History</source>
+        <translation>Historial</translation>
+    </message>
+    <message>
+        <location filename="../../providers.cpp" line="801"/>
+        <source>Mathematics</source>
+        <translation>Matemàtiques</translation>
+    </message>
+</context>
+</TS>
