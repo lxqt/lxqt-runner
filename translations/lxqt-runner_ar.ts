984de33a523b12cddc1b66b1238fd77e62e4d7e2
ButterflyOfFire <butterflyoffire+lxqt@protonmail.com>
2018-07-10 17:37:38 +0000

    Translated using Weblate (Arabic)
    Currently translated at 76.1% (16 of 21 strings)
    
    Translation: LXQt/LXQt Runner
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-runner/ar/

--- translations/lxqt-runner_ar.ts
+++ translations/lxqt-runner_ar.ts
@@ -84,7 +84,7 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>مُطلق التّطبيقات</translation>
+        <translation>مُطلق التّطبيقات </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="93"/>
