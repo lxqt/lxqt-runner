74ad59bad003891df19a7eb77c3600ab5c33b4cc
p-bo <pavel.borecki@gmail.com>
2018-07-11 06:39:16 +0000

    Translated using Weblate (Čeština)
    Currently translated at 100,0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Runner
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-runner/cs/

--- translations/lxqt-runner_cs.ts
+++ translations/lxqt-runner_cs.ts
@@ -84,7 +84,7 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Spouštěč programů</translation>
+        <translation>Spouštěč programů </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="93"/>
