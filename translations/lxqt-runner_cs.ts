5101f0c572851293fae3cde1b87de226b863e198
p-bo <pavel.borecki@gmail.com>
2018-07-16 13:48:52 +0000

    Translated using Weblate (Čeština)
    Currently translated at 100,0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Runner
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-runner/cs/

--- translations/lxqt-runner_cs.ts
+++ translations/lxqt-runner_cs.ts
@@ -36,7 +36,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;Upozornění&lt;/b&gt;: Velikost obslužných prvků nahoře je omezena na 2/3 výšky a šířky plochy.</translation>
+        <translation>&lt;b&gt;Upozornění&lt;/b&gt;: Velikost obslužných prvků v nejvyšší úrovni je omezena na 2/3 výšky a šířky plochy.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="63"/>
@@ -84,7 +84,7 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Spouštěč programů </translation>
+        <translation>Spouštěč aplikací </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="93"/>
@@ -99,7 +99,7 @@
     <message>
         <location filename="../dialog.cpp" line="350"/>
         <source>Show/hide runner dialog</source>
-        <translation>Ukázat/skrýt dialog spouštěče programů</translation>
+        <translation>Zobrazit/skrýt dialog spouštěče programů</translation>
     </message>
 </context>
 <context>
