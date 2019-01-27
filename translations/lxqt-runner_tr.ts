52f8abc8ee2354194e15f49a4835755532f9ebc4
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2019-01-27 04:02:42 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100,0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Runner
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-runner/tr/

--- translations/lxqt-runner_tr.ts
+++ translations/lxqt-runner_tr.ts
@@ -26,27 +26,27 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="46"/>
         <source>Show history first</source>
-        <translation type="unfinished"></translation>
+        <translation>Önce geçmişi göster</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="53"/>
         <source>Store/show history</source>
-        <translation type="unfinished"></translation>
+        <translation>Geçmişi kaydet/göster</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Not&lt;/b&gt;: Üst seviye gereçlerin boyutu masaüstü yükseklik ve genişliğinin 2/3 ü ile  sınırlıdır.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="63"/>
         <source>Show list with:</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste genişliğini göster:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="70"/>
         <source> item(s)</source>
-        <translation type="unfinished"></translation>
+        <translation> öge(ler)</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="90"/>
