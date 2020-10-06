700de95903796c4654df79dbed8bee2cffebd071
Bittor Poza <bipoza@gmail.com>
2020-10-06 21:06:00 +0200

    Weblate commits
    Co-authored-by: Bittor Poza <bipoza@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/eu/
    Translation: LXQt Desktop/lxqt-runner

--- translations/lxqt-runner_eu.ts
+++ translations/lxqt-runner_eu.ts
@@ -26,22 +26,22 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="46"/>
         <source>Show history first</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Erakutsi lehenengo historia</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="53"/>
         <source>Store/show history</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Gorde/ikusi historia</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;b&gt;Oharra&lt;/b&gt;: Goi mailako widgeten tamaina mahaigainaren altuera eta zabaleraren 2/3era mugatua dago.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="63"/>
         <source>Show list with:</source>
-        <translation type="unfinished"></translation>
+        <translation>Erakutsi zerrenda honekin:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="70"/>
@@ -56,17 +56,17 @@
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Pantailaren goiko ertza</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Pantailaren erdigunea</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="68"/>
         <source>Focused screen</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Enfokatutako pantaila</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="72"/>
@@ -76,7 +76,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="81"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Berrezarri</translation>
     </message>
 </context>
 <context>
@@ -84,17 +84,17 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Aplikazio-abiarazlea</translation>
+        <translation>Aplikazio-abiarazlea </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="102"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfiguratu</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="106"/>
         <source>Clear History</source>
-        <translation type="unfinished"></translation>
+        <translation>Garbitu historia</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="359"/>
