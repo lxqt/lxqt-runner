bc2d36df1cac8edd5e1ebf08ca502c182844d459
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-07 18:26:05 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/bg/
    Translation: LXQt Desktop/LXQt-runner

---
+++ translations/lxqt-runner_bg.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Настройки runner</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Външен вид</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>Позиция:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="36"/>
+        <source>Show on:</source>
+        <translation>Показване на:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="46"/>
+        <source>Show history first</source>
+        <translation>Първо покажи историята</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="53"/>
+        <source>Store/show history</source>
+        <translation>Запази/покажи историята</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="60"/>
+        <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
+        <translation>&lt;b&gt;Note&lt;/b&gt;: Размерът на приставките от високо ниво е ограничен до 2/3 от височината и ширината на работния плот.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="63"/>
+        <source>Show list with:</source>
+        <translation>Покажи списъка с:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="70"/>
+        <source> item(s)</source>
+        <translation> елементи</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="90"/>
+        <source>Shortcut:</source>
+        <translation>Клавишна комбинация:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Top edge of the screen</source>
+        <translation>Горна граница на екрана</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="62"/>
+        <source>Center of the screen</source>
+        <translation>В центъра на екрана</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation>Активен екран</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation>Винаги на %1 екран</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation>Нулиране</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation>Стартер на приложение </translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="105"/>
+        <source>Configure</source>
+        <translation>Настройване</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="109"/>
+        <source>Clear History</source>
+        <translation>Изчистване на историята</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="370"/>
+        <source>Show/hide runner dialog</source>
+        <translation>Показване/скриване диалог на runner</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../providers.cpp" line="445"/>
+        <source>History</source>
+        <translation>История</translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="845"/>
+        <source>Mathematics</source>
+        <translation>Математика</translation>
+    </message>
+</context>
+</TS>
