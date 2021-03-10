66764985bc46621ee388eff2c6c2de71be103189
Hasan Yıldız <hasanyildiz0@yaani.com>
2021-03-10 19:06:56 +0100

    Weblate commits
    Co-authored-by: Hasan Yıldız <hasanyildiz0@yaani.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/tr/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_tr.ts
+++ translations/lxqt-runner_tr.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Hesaplama sonucunu panoya kopyala</translation>
     </message>
 </context>
 </TS>
