2a032da33b1fa6d0f6e14400c9596c6770b8c99d
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-11 07:18:57 +0100

    Translations update from Weblate (#181)
    * Weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    
    * Weblate commits
    
    Co-authored-by: Hasan Yıldız <hasanyildiz0@yaani.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/tr/
    Translation: LXQt Desktop/LXQt-runner
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Hasan Yıldız <hasanyildiz0@yaani.com>

--- translations/lxqt-runner_si.ts
+++ translations/lxqt-runner_si.ts
@@ -1,82 +1,82 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="tr">
+<TS version="2.1" language="si">
 <context>
     <name>ConfigureDialog</name>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation>Çalıştırıcı Ayarları</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation>Görünüm</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>Konumlandırma:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Show on:</source>
-        <translation>Şunda Göster:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="46"/>
         <source>Show history first</source>
-        <translation>Önce geçmişi göster</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="53"/>
         <source>Store/show history</source>
-        <translation>Geçmişi kaydet/göster</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;Not&lt;/b&gt;: Üst seviye gereçlerin boyutu masaüstü yükseklik ve genişliğinin 2/3 ü ile sınırlıdır.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="63"/>
         <source>Show list with:</source>
-        <translation>Liste genişliğini göster:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="70"/>
         <source> item(s)</source>
-        <translation> öge(ler)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="90"/>
         <source>Shortcut:</source>
-        <translation>Kısayol:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation>Ekranın üst köşesi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation>Ekranın merkezi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation>Odaklanmış ekran</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation>Her zaman %1 ekranında</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation>Sıfırla</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -84,22 +84,22 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Uygulama başlatıcı </translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="105"/>
         <source>Configure</source>
-        <translation>Yapılandır</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="109"/>
         <source>Clear History</source>
-        <translation>Geçmişi Temizle</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="370"/>
         <source>Show/hide runner dialog</source>
-        <translation>Çalıştırıcı penceresini göster/gizle</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -107,12 +107,12 @@
     <message>
         <location filename="../providers.cpp" line="445"/>
         <source>History</source>
-        <translation>Geçmiş</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation>Hesaplama sonucunu panoya kopyala</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
