95525be5cf19adc6fc2a7b4d309f4ea6de9b3518
zoli111 <acszoltan111@gmail.com>
2021-04-10 09:20:41 +0200

    Weblate commits
    Co-authored-by: zoli111 <acszoltan111@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/hu/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_hu.ts
+++ translations/lxqt-runner_hu.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Másolja a vágólapra a számolás eredményét</translation>
     </message>
 </context>
 </TS>
