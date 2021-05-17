35630a913a54536e5a5291cd98112912bbd3d346
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-05-17 08:27:22 +0200

    Weblate commits (#196)
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/vi/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name
    
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>

---
+++ translations/lxqt-runner_vi.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Cài đặt Trình chạy</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Hình thức</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>Vị trí:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="36"/>
+        <source>Show on:</source>
+        <translation>Hiện tại:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="46"/>
+        <source>Show history first</source>
+        <translation>Hiển thị lịch sử trước</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="53"/>
+        <source>Store/show history</source>
+        <translation>Lưu trữ/hiển thị lịch sử</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="60"/>
+        <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
+        <translation>&lt;b&gt;Chú ý&lt;/b&gt;: Kích cỡ các widget ở trên cùng bị giới hạn theo 2/3 chiều cao và chiều rộng của bàn làm việc.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="63"/>
+        <source>Show list with:</source>
+        <translation>Hiện danh sách với:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="70"/>
+        <source> item(s)</source>
+        <translation> mục</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="90"/>
+        <source>Shortcut:</source>
+        <translation>Lối tắt:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Top edge of the screen</source>
+        <translation>Mép trên của màn hình</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="62"/>
+        <source>Center of the screen</source>
+        <translation>Trung tâm màn hình</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation>Màn hình tập trung</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation>Luôn hiện trên màn hình %1</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation>Làm lại</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation>Trình chạy ứng dụng </translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="105"/>
+        <source>Configure</source>
+        <translation>Tùy chỉnh</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="109"/>
+        <source>Clear History</source>
+        <translation>Xóa Lịch sử</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="370"/>
+        <source>Show/hide runner dialog</source>
+        <translation>Hiện/ẩn hộp thoại trình chạy</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../providers.cpp" line="445"/>
+        <source>History</source>
+        <translation>Lịch sử</translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="846"/>
+        <source>Copy calculation result to clipboard</source>
+        <translation>Chép kết quả tính toán vào khay nhớ tạm</translation>
+    </message>
+</context>
+</TS>
