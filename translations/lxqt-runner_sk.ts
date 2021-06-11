943b2e396f7e40e78c6edb3ca4fcc4df017cdbdd
pavol polonec <pavolpolonec@gmail.com>
2021-06-11 13:07:30 +0200

    Weblate commits
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/sk/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_sk.ts
+++ translations/lxqt-runner_sk.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Skopírovať výsledok výpočtu do schránky</translation>
     </message>
 </context>
 </TS>
