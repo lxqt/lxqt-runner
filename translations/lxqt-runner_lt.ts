682156afb69954411633198d61397389b36f6ce9
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-07-08 19:17:24 +0200

    Weblate commits (#201)
    Co-authored-by: Moo <hazap@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/lt/
    Translation: LXQt Desktop/LXQt-runner
    
    Co-authored-by: Moo <hazap@hotmail.com>

--- translations/lxqt-runner_lt.ts
+++ translations/lxqt-runner_lt.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopijuoti skaičiavimo rezultatą į iškarpinę</translation>
     </message>
 </context>
 </TS>
