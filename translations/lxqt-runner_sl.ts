8d01f0aaa7503015ce20e2edda72f15e3669f0e8
Weblate <noreply@weblate.org>
2022-02-13 05:08:59 +0100

    Weblate commits
    Co-authored-by: Dejweed <12davidkac@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/sl/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name

--- translations/lxqt-runner_sl.ts
+++ translations/lxqt-runner_sl.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiraj rezultat v odložišče</translation>
     </message>
 </context>
 </TS>
