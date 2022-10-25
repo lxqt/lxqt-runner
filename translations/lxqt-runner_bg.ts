dd1dd247974a92c93e2db53aa2cc85bf89909c30
Weblate <noreply@weblate.org>
2022-10-25 10:10:31 +0200

    Weblate commits
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Co-authored-by: wallon <franckjl@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/uk/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_bg.ts
+++ translations/lxqt-runner_bg.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="80"/>
         <source>Clear input on launching</source>
-        <translation type="unfinished"></translation>
+        <translation>Изчистване на въведеното за стартиране</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="97"/>
