986eb65ba79c5a070557ff66b01455c06fcc29a7
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-10-28 14:09:28 +0800

    Weblate commits (#229)
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/tr/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_et.ts
+++ translations/lxqt-runner_et.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="80"/>
         <source>Clear input on launching</source>
-        <translation type="unfinished"></translation>
+        <translation>Käivitamisel kustuta sisestatud andmed</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="97"/>
--- translations/lxqt-runner_tr.ts
+++ translations/lxqt-runner_tr.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="80"/>
         <source>Clear input on launching</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlangıçta girişi temizle</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="97"/>
