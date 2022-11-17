bb052465cbf3a767dc3ccdbc9859d59d3c023825
Weblate <noreply@weblate.org>
2022-11-17 23:10:39 +0100

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Carl Andersen <carl@flying-cloud.dk>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ru/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_ru.ts
+++ translations/lxqt-runner_ru.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="80"/>
         <source>Clear input on launching</source>
-        <translation type="unfinished"></translation>
+        <translation>Очищать поле при запуске</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="97"/>
