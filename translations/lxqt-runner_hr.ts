437216d6a18d718c7e488f616c157cf592f37cdd
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-05-16 21:11:27 +0200

    Weblate commits (#253)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/hr/
    Translation: LXQt Desktop/LXQt-runner
    
    Co-authored-by: milotype <mail@milotype.de>

--- translations/lxqt-runner_hr.ts
+++ translations/lxqt-runner_hr.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="80"/>
         <source>Clear input on launching</source>
-        <translation type="unfinished"></translation>
+        <translation>Izbriši unos pri pokretanju</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="97"/>
