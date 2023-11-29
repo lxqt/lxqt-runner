18ad2f558009266a7a0412cf886677f185007a72
Weblate <noreply@weblate.org>
2023-11-29 11:22:51 +0100

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Paolo Vigoroso <xpaolox@zoznam.sk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/sk/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_ru.ts
+++ translations/lxqt-runner_ru.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Top margin</source>
-        <translation type="unfinished"></translation>
+        <translation>Верхний отступ</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="43"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> пкс</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="50"/>
