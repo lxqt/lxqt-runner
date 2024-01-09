2d64f222e12082e72a74efec4b3a0994a4d510a6
Weblate <noreply@weblate.org>
2024-01-09 21:13:42 +0100

    Weblate commits
    Co-authored-by: Sensalire <kbirabwa@yahoo.co.uk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/lg/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name

---
+++ translations/lxqt-runner_lg.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Enteekateeka za LXQt-runner</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Endabika</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>W&apos;erabikira:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="36"/>
+        <source>Top margin</source>
+        <translation>Akabanga k&apos;elekawo waggulu</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="43"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="50"/>
+        <source>Show on:</source>
+        <translation>Olutimbe kw&apos;erabikira:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="60"/>
+        <source>Show history first</source>
+        <translation>Ebyasemba okutandikibwa bye biba bisookanga ku lukalala</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="67"/>
+        <source>Store/show history</source>
+        <translation>Ebeere n&apos;olukalala lwa byeyasemba okutandika</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="74"/>
+        <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
+        <translation>&lt;b&gt;Genderera&lt;/b&gt;: Olukalala luno lusibidwa butasukka 2/3 eby&apos;obuwanvu n&apos;obugazi bw&apos;olutimbe.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="77"/>
+        <source>Show list with:</source>
+        <translation>Bungi bw&apos;ebirabikira ku lukalala:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="84"/>
+        <source> item(s)</source>
+        <translation> (ebintu)</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="94"/>
+        <source>Clear input on launching</source>
+        <translation>Etandikenga w&apos;owandikira wereere</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="111"/>
+        <source>Shortcut:</source>
+        <translation>Amapeesa agagitandika:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Top edge of the screen</source>
+        <translation>Ebeere ku njegoyego ey&apos;olutimbe eya waggulu</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="62"/>
+        <source>Center of the screen</source>
+        <translation>Ebeere mu makkati g&apos;olutimbe</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="67"/>
+        <source>Focused screen</source>
+        <translation>Ejjenga ku lutimbe olukolerwamu</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="71"/>
+        <source>Always on screen %1</source>
+        <translation>Ejjenga ku lutimbe namba %1</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="80"/>
+        <source>Reset</source>
+        <translation>Zaawo ebyasooka</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation>Puloguramu etandika zinnaayo </translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="106"/>
+        <source>Configure</source>
+        <translation>Enteekateeka</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="110"/>
+        <source>Clear History</source>
+        <translation>Olukalala lusazemu</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="409"/>
+        <source>Show/hide runner dialog</source>
+        <translation>Laga/kisa akaboozi ka LXQt-runner</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../providers.cpp" line="445"/>
+        <source>History</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="846"/>
+        <source>Copy calculation result to clipboard</source>
+        <translation>Ebibalidwa bigende awaterekebwa ebikoppedwa</translation>
+    </message>
+</context>
+</TS>
