8ef46391e241f7b3a41338c0c97d79906366e411
Weblate <noreply@weblate.org>
2024-06-17 20:02:42 +0200

    Weblate commits
    Co-authored-by: Peter  Jespersen <flywheel@illogical.dk>
    Co-authored-by: Pjetur G. Hjaltason <pjetur@pjetur.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/is/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/da/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name

--- translations/lxqt-runner_is.ts
+++ translations/lxqt-runner_is.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="da">
+<TS version="2.1" language="is">
 <context>
     <name>ConfigureDialog</name>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation>Indstillinger for Programstarter</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation>Udseende</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>Positionering:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Top margin</source>
-        <translation>Topmargen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="43"/>
         <source> px</source>
-        <translation> px</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="50"/>
         <source>Show on:</source>
-        <translation>Vis på:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>Show history first</source>
-        <translation>Vis historik først</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="67"/>
         <source>Store/show history</source>
-        <translation>Gem/vis historik</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="74"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;Bemærk&lt;/b&gt;: Størrelsen af top-niveau widgets er begrænset til 2/3 af skrivebordets højde og bredde.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="77"/>
         <source>Show list with:</source>
-        <translation>Vis liste med:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="84"/>
         <source> item(s)</source>
-        <translation> element(er)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="94"/>
         <source>Clear input on launching</source>
-        <translation>Ryd input ved opstart</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="111"/>
         <source>Shortcut:</source>
-        <translation>Genvej:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation>Øverste kant af skærmen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation>I midten af skærmen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation>Skærm som har fokus</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation>Altid på skærm %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation>Nulstil</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -99,22 +99,22 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Programstarter </translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="106"/>
         <source>Configure</source>
-        <translation>Indstil</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="110"/>
         <source>Clear History</source>
-        <translation>Ryd historik</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="409"/>
         <source>Show/hide runner dialog</source>
-        <translation>Vis/skjul Programstarter-dialog</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../providers.cpp" line="445"/>
         <source>History</source>
-        <translation>Historik</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation>Kopiér udregningsresultat til udklipsholder</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
