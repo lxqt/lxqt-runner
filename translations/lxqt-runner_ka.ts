6c7b0e38691db5c42b2eeb92d61fd49e1e11854f
Weblate <noreply@weblate.org>
2025-01-02 08:24:29 +0100

    Weblate commits
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ko/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name

--- translations/lxqt-runner_ka.ts
+++ translations/lxqt-runner_ka.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="ka">
 <context>
     <name>ConfigureDialog</name>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation>실행도구 설정</translation>
+        <translation>გამშვების მორგება</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation>모양새</translation>
+        <translation>გარეგნობა</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>위치 지정:</translation>
+        <translation>მდებარეობა:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Top margin</source>
-        <translation>상단 여백</translation>
+        <translation>ზედა მინდორი</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="43"/>
         <source> px</source>
-        <translation> px</translation>
+        <translation> პქს</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="50"/>
         <source>Show on:</source>
-        <translation>표시 대상:</translation>
+        <translation>ჩვენება რაზე:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>Show history first</source>
-        <translation>먼저 히스토리 표시하기</translation>
+        <translation>ჯერ ისტორიის ჩვენება</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="67"/>
         <source>Store/show history</source>
-        <translation>히스토리 보관/표시하기</translation>
+        <translation>ისტორიის დამახსოვრება/ჩვენება</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="74"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;참고&lt;/b&gt;: 최상위 위젯의 크기는 바탕화면 높이와 너비의 2/3으로 제한됩니다.</translation>
+        <translation>&lt;b&gt;შენიშვნა&lt;/b&gt;: ზედა დონის ვიჯეტები შეზღუდულია სამუშაო მაგიდის სიგრძე სიგანის 2/3-ით.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="77"/>
         <source>Show list with:</source>
-        <translation>다음으로 목록 표시하기:</translation>
+        <translation>სიის ჩვენება რით:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="84"/>
         <source> item(s)</source>
-        <translation> 개 항목</translation>
+        <translation> ელემენტი</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="94"/>
         <source>Clear input on launching</source>
-        <translation>실행 시 입력 지우기</translation>
+        <translation>შეყვანის გასუფთავება გაშვებისას</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="111"/>
         <source>Shortcut:</source>
-        <translation>단축키:</translation>
+        <translation>მალსახმობი:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation>화면 상단 가장자리</translation>
+        <translation>ეკრანის ზედა წიბო</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation>화면 중앙</translation>
+        <translation>ეკრანის ცენტრი</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation>초점이 맞춰진 화면</translation>
+        <translation>ფოკუსის მქონე ეკრანი</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation>항상 %1 화면에 표시</translation>
+        <translation>ყოველთვის ეკრანზე %1</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation>재설정</translation>
+        <translation>ჩამოყრა</translation>
     </message>
 </context>
 <context>
@@ -99,22 +99,22 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>응용프로그램 실행 프로그램 </translation>
+        <translation>აპლიკაციების გამშვები </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="109"/>
         <source>Configure</source>
-        <translation>구성하기</translation>
+        <translation>მორგება</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="113"/>
         <source>Clear History</source>
-        <translation>히스토리 비우기</translation>
+        <translation>ისტორიის გასუფთავება</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="465"/>
         <source>Show/hide runner dialog</source>
-        <translation>실행도구 대화창 표시/숨김</translation>
+        <translation>გამშვების დიალოგის ჩვენება/დამალვა</translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../providers.cpp" line="445"/>
         <source>History</source>
-        <translation>히스토리</translation>
+        <translation>ისტორია</translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation>클립보드에 계산 결과 복사하기</translation>
+        <translation>გამოთვლის შედეგის კოპირება ბუფერში</translation>
     </message>
 </context>
 </TS>
--- translations/lxqt-runner_kab.ts
+++ translations/lxqt-runner_kab.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="kab">
 <context>
     <name>ConfigureDialog</name>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation>실행도구 설정</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation>모양새</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>위치 지정:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Top margin</source>
-        <translation>상단 여백</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="43"/>
         <source> px</source>
-        <translation> px</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="50"/>
         <source>Show on:</source>
-        <translation>표시 대상:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="60"/>
         <source>Show history first</source>
-        <translation>먼저 히스토리 표시하기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="67"/>
         <source>Store/show history</source>
-        <translation>히스토리 보관/표시하기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="74"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;참고&lt;/b&gt;: 최상위 위젯의 크기는 바탕화면 높이와 너비의 2/3으로 제한됩니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="77"/>
         <source>Show list with:</source>
-        <translation>다음으로 목록 표시하기:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="84"/>
         <source> item(s)</source>
-        <translation> 개 항목</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="94"/>
         <source>Clear input on launching</source>
-        <translation>실행 시 입력 지우기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="111"/>
         <source>Shortcut:</source>
-        <translation>단축키:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation>화면 상단 가장자리</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation>화면 중앙</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="67"/>
         <source>Focused screen</source>
-        <translation>초점이 맞춰진 화면</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>Always on screen %1</source>
-        <translation>항상 %1 화면에 표시</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="80"/>
         <source>Reset</source>
-        <translation>재설정</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -99,22 +99,22 @@
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>응용프로그램 실행 프로그램 </translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="109"/>
         <source>Configure</source>
-        <translation>구성하기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="113"/>
         <source>Clear History</source>
-        <translation>히스토리 비우기</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="465"/>
         <source>Show/hide runner dialog</source>
-        <translation>실행도구 대화창 표시/숨김</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../providers.cpp" line="445"/>
         <source>History</source>
-        <translation>히스토리</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation>클립보드에 계산 결과 복사하기</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
