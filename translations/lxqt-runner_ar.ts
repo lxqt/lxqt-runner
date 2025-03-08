1a41a05a8d87cf4e0c9125124815cc073e2de816
Weblate <noreply@weblate.org>
2025-03-08 23:25:41 +0100

    Weblate commits
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/zh_TW/
    Translation: LXQt Desktop/LXQt-runner

--- translations/lxqt-runner_ar.ts
+++ translations/lxqt-runner_ar.ts
@@ -37,7 +37,7 @@
         <location filename="../configuredialog/configuredialog.ui" line="57"/>
         <source>With multiple screens, if the window is shown on the focused screen,
 this value is only for the smallest screen. The minimum width is 400 px.</source>
-        <translation type="unfinished"></translation>
+        <translation>في حالة وجود شاشات متعددة، إذا تم عرض النافذة على الشاشة المحددة، فإن هذه القيمة مخصصة فقط للشاشة الأصغر. الحد الأدنى للعرض هو 400 بكسل.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="61"/>
@@ -98,7 +98,7 @@ this value is only for the smallest screen. The minimum width is 400 px.</source
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>On Wayland, the shortcut has to be added
 in the compositor settings for &apos;lxqt-runner&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>في Wayland، يجب إضافة الاختصار في إعدادات الملحن لـ &apos;lxqt-runner&apos;.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="83"/>
