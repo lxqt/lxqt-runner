836066aaa16cfe60345161f4ec692cfc4f74fb73
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-21 08:04:27 +0200

    Weblate commits (#323)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/sv/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name
    
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: milotype <mail@milotype.de>

--- translations/lxqt-runner_sv.ts
+++ translations/lxqt-runner_sv.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hr">
+<TS version="2.1" language="sv">
 <context>
     <name>ConfigureDialog</name>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="14"/>
         <source>Runner Settings</source>
-        <translation>Pokretač – Postavke</translation>
+        <translation>Inställningar för programstartaren</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="20"/>
         <source>Appearance</source>
-        <translation>Izgled</translation>
+        <translation>Utseende</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="26"/>
         <source>Positioning:</source>
-        <translation>Pozicioniranje:</translation>
+        <translation>Placering:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="36"/>
         <source>Top margin</source>
-        <translation>Gornja margina</translation>
+        <translation>Övre marginal</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="43"/>
@@ -31,14 +31,14 @@
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="50"/>
         <source>Width:</source>
-        <translation>Širina:</translation>
+        <translation>Bredd:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="57"/>
         <source>With multiple screens, if the window is shown on the focused screen,
 this value is only for the smallest screen. The minimum width is 400 px.</source>
-        <translation>S više ekrana, ako se prozor prikazuje na fokusiranom ekranu,
-ova vrijednost vrijedi samo za najmanji ekran. Minimalna širina je 400 px.</translation>
+        <translation>Vid använding av flera skärmar: om fönstret visas på den aktiva skärmen,
+så gäller värdet bara för den minsta skärmen. Minsta bredd är 400 px.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="61"/>
@@ -48,74 +48,74 @@ ova vrijednost vrijedi samo za najmanji ekran. Minimalna širina je 400 px.</tra
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="71"/>
         <source>Show on:</source>
-        <translation>Prikaži na:</translation>
+        <translation>Visa på:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="108"/>
         <source>Show history first</source>
-        <translation>Najprije prikaži povijest</translation>
+        <translation>Visa historik först</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="101"/>
         <source>Store/show history</source>
-        <translation>Spremi/prikaži povijest</translation>
+        <translation>Lagra/visa historik</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="81"/>
         <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
-        <translation>&lt;b&gt;Napomena&lt;/b&gt;: Veličina programčića najviše razine ograničena je na 2/3 visine i širine radne površine.</translation>
+        <translation>&lt;b&gt;Obs&lt;/b&gt;: Storleken på widgeten är begränsad till 2/3 av skrivbordets höjd och bredd.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="84"/>
         <source>Show list with:</source>
-        <translation>Prikaži popis s:</translation>
+        <translation>Visa lista med:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="91"/>
         <source> item(s)</source>
-        <translation> stavke(i)</translation>
+        <translation> objekt</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="115"/>
         <source>Clear input on launching</source>
-        <translation>Izbriši unos pri pokretanju</translation>
+        <translation>Rensa inmatning vid uppstart</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.ui" line="132"/>
         <source>Shortcut:</source>
-        <translation>Prečac:</translation>
+        <translation>Kortkommando:</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="61"/>
         <source>Top edge of the screen</source>
-        <translation>Gornji rub ekrana</translation>
+        <translation>Skärmens övre kant</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="62"/>
         <source>Center of the screen</source>
-        <translation>Sredina ekrana</translation>
+        <translation>Mitt på skärmen</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="71"/>
         <source>On Wayland, the shortcut has to be added
 in the compositor settings for &apos;lxqt-runner&apos;.</source>
-        <translation>Na Waylandu se prečac mora dodati
-u postavkama kompozitora za &apos;lxqt-runner&apos;.</translation>
+        <translation>För Wayland måste kortkommandot läggas till 
+i kompositorsinställningarna för &apos;lxqt-runner&apos;.</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="83"/>
         <source>Focused screen</source>
-        <translation>Aktivan ekran</translation>
+        <translation>Aktiv skärm</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="87"/>
         <source>Always on screen %1</source>
-        <translation>Uvijek na ekranu %1</translation>
+        <translation>skärm %1 (alltid)</translation>
     </message>
     <message>
         <location filename="../configuredialog/configuredialog.cpp" line="96"/>
         <source>Reset</source>
-        <translation>Resetiraj</translation>
+        <translation>Återställ</translation>
     </message>
 </context>
 <context>
@@ -123,22 +123,22 @@ u postavkama kompozitora za &apos;lxqt-runner&apos;.</translation>
     <message>
         <location filename="../dialog.ui" line="26"/>
         <source>Application launcher </source>
-        <translation>Pokretač programa </translation>
+        <translation>Programstartare </translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="109"/>
         <source>Configure</source>
-        <translation>Konfiguriraj</translation>
+        <translation>Konfigurera</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="113"/>
         <source>Clear History</source>
-        <translation>Izbriši povijest</translation>
+        <translation>Rensa historik</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="523"/>
         <source>Show/hide runner dialog</source>
-        <translation>Prikaži/sakrij dijalog pokretača</translation>
+        <translation>Visa/dölj programstartare</translation>
     </message>
 </context>
 <context>
@@ -146,12 +146,12 @@ u postavkama kompozitora za &apos;lxqt-runner&apos;.</translation>
     <message>
         <location filename="../providers.cpp" line="445"/>
         <source>History</source>
-        <translation>Povijest</translation>
+        <translation>Historik</translation>
     </message>
     <message>
         <location filename="../providers.cpp" line="846"/>
         <source>Copy calculation result to clipboard</source>
-        <translation>Kopiraj rezultat izračuna u međuspremnik</translation>
+        <translation>Kopiera det uträknade resultatet till urklipp</translation>
     </message>
 </context>
 </TS>
