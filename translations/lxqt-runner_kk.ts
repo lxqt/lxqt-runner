6edbdf3bfa7d7cb6770ae36958d2d76cd9393a18
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2026-01-20 18:47:01 +0100

    Weblate commits (#335)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner-module-name/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-runner/kk/
    Translation: LXQt Desktop/LXQt-runner
    Translation: LXQt Desktop/LXQt-runner Module Name
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>

---
+++ translations/lxqt-runner_kk.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="14"/>
+        <source>Runner Settings</source>
+        <translation>Жөнелткіш баптаулары</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Сыртқы түрі</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="26"/>
+        <source>Positioning:</source>
+        <translation>Орналасуы:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="36"/>
+        <source>Top margin</source>
+        <translation>Жоғарғы жиек</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="43"/>
+        <source> px</source>
+        <translation> пикс</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="50"/>
+        <source>Width:</source>
+        <translation>Ені:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="57"/>
+        <source>With multiple screens, if the window is shown on the focused screen,
+this value is only for the smallest screen. The minimum width is 400 px.</source>
+        <translation>Бірнеше экран болған кезде, егер терезе фокусталған экранда көрсетілсе,
+бұл мән тек ең кіші экран үшін қолданылады. Ең кіші ені — 400 пикс.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="61"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="71"/>
+        <source>Show on:</source>
+        <translation>Келесіде көрсету:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="108"/>
+        <source>Show history first</source>
+        <translation>Алдымен тарихты көрсету</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="101"/>
+        <source>Store/show history</source>
+        <translation>Тарихты сақтау/көрсету</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="81"/>
+        <source>&lt;b&gt;Note&lt;/b&gt;: The size of top-level widgets are constrained to 2/3 of the desktop&apos;s height and width.</source>
+        <translation>&lt;b&gt;Ескертпе&lt;/b&gt;: Жоғарғы деңгейлі виджеттердің өлшемі жұмыс үстелінің биіктігі мен енінің 2/3 бөлігіне дейін шектелген.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="84"/>
+        <source>Show list with:</source>
+        <translation>Тізімді көрсету:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="91"/>
+        <source> item(s)</source>
+        <translation> элемент</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="115"/>
+        <source>Clear input on launching</source>
+        <translation>Іске қосқан кезде енгізу өрісін тазарту</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.ui" line="132"/>
+        <source>Shortcut:</source>
+        <translation>Жарлық:</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="61"/>
+        <source>Top edge of the screen</source>
+        <translation>Экранның жоғарғы жиегі</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="62"/>
+        <source>Center of the screen</source>
+        <translation>Экранның ортасы</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="71"/>
+        <source>On Wayland, the shortcut has to be added
+in the compositor settings for &apos;lxqt-runner&apos;.</source>
+        <translation>Wayland жүйесінде &apos;lxqt-runner&apos; үшін жарлық
+композитор баптауларында қосылуы тиіс.</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="83"/>
+        <source>Focused screen</source>
+        <translation>Фокусталған экран</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="87"/>
+        <source>Always on screen %1</source>
+        <translation>Әрқашан %1 экранында</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog/configuredialog.cpp" line="96"/>
+        <source>Reset</source>
+        <translation>Тастау</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="26"/>
+        <source>Application launcher </source>
+        <translation>Қолданбаларды іске қосқыш </translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="109"/>
+        <source>Configure</source>
+        <translation>Баптау</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="113"/>
+        <source>Clear History</source>
+        <translation>Тарихты тазарту</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="523"/>
+        <source>Show/hide runner dialog</source>
+        <translation>Жөнелткіш сұхбатын көрсету/жасыру</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../providers.cpp" line="445"/>
+        <source>History</source>
+        <translation>Тарихы</translation>
+    </message>
+    <message>
+        <location filename="../providers.cpp" line="846"/>
+        <source>Copy calculation result to clipboard</source>
+        <translation>Есептеу нәтижесін буферге көшіру</translation>
+    </message>
+</context>
+</TS>
